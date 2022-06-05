62c5837148235aeea3b9074cce7a8680c945a6ce
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-06-05 17:45:40 +0300

    comment form

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from instagram.models import Image, Profile
+from instagram.models import Image, Profile,Comment
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Image)
+admin.site.register(Comment)
--- instagram/forms.py
+++ instagram/forms.py
@@ -25,4 +25,17 @@ class UpdateUserForm(forms.ModelForm):
 class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('name', 'profile_photo', 'bio')          
+        fields = ('name', 'profile_photo', 'bio') 
+
+
+
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)               
